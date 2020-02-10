9f074f2d86ed8b02820fb2d7896e8ac56b95680d
bfo1056 <60587940+bfo1056@users.noreply.github.com>
2020-02-10 14:11:18 +1300

    hello world program

---
+++ hello world.py
@@ -0,0 +1,14 @@
+hello="hello"
+
+name="john"
+
+last_name="smith"
+
+print(hello, name , last_name) 
+
+
+age=18
+print("you are", age, "years old")
+
+
+fav_animal= input("what is your favorite animal?")
