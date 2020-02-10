eeae309a9502491833cc9c13638f91f29e5e1d1e
bfo1056 <60587940+bfo1056@users.noreply.github.com>
2020-02-10 14:10:21 +1300

    Madlib

---
+++ madlib.py
@@ -0,0 +1,8 @@
+print("hello welcome to my madlib program")
+name = input("enter name")
+bodypart = input("enter body part")
+illness = input("enter illness")
+subject = input("enter subject")
+number = input("enter a number from 1 to 5")
+
+print("please excuse {} from {} today he is suffering from a bad case of {} which has caused a lot of {} pain.{} will be back at school in {} days".format(name, subject, illness, bodypart, name, number))
